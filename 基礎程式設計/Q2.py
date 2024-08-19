f1b6ce7b912083dfb0738553bec53282b11ff16f
chung <chung7113040006@gmail.com>
2024-08-19 17:01:14 +0800

    Q2Q3練習

---
+++ 基礎程式設計/Q2.py
@@ -0,0 +1,3 @@
+c=float(input("enter the tempature"))
+f=c*9/5+32
+print("the tempatureis"+ "%.2f"%f)
---
+++ 基礎程式設計/Q3.py
@@ -0,0 +1,4 @@
+m,n = map(int,input("m n").split())#map輸入我們需要的事情 以及輸入的值 用sp作切割 sp預設切割鍵為空白鍵
+print(m+n)
+print(m-n)
+print(m*n)
